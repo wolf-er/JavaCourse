d0d1de4cedd189fff9ed2d005479ffeab298313c
gatsko <Age12345>
2018-07-10 17:54:51 +0300

    HW

--- App/src/ru/gatsko/edu/java2/collections/PhoneBook.java
+++ App/src/ru/gatsko/edu/java2/collections/PhoneBook.java
@@ -6,15 +6,14 @@ import java.util.HashSet;
 public class PhoneBook {
     private HashMap<String, HashSet<String>> book = new HashMap<String, HashSet<String>>();
     public void add(String name, String phone){
+        HashSet<String> phones = new HashSet<String>();
         if (book.containsKey(name)){
-            HashSet<String> phones = book.get(name);
+            phones = book.get(name);
             phones.add(phone);
             book.put(name, phones);
-        } else {
-            HashSet<String> phones = new HashSet<String>();
-            phones.add(phone);
-            book.put(name, phones);
-        };
+        }
+        phones.add(phone);
+        book.put(name, phones);;
     }
     public HashSet<String> get(String name){
         if (book.containsKey(name)){
